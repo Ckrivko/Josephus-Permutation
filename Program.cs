242a1f59ca16d568b92bbd834c3e24373037a45b
Ckrivko <76474570+Ckrivko@users.noreply.github.com>
2022-06-30 00:08:33 +0300

    Add files via upload
    This problem takes its name by arguably the most important event in the life of the ancient historian Josephus: according to his tale, he and his 40 soldiers were trapped in a cave by the Romans during a siege.
    
    Refusing to surrender to the enemy, they instead opted for mass suicide, with a twist: they formed a circle and proceeded to kill one man every three, until one last man was left (and that it was supposed to kill himself to end the act).
    
    Well, Josephus and another man were the last two and, as we now know every detail of the story, you may have correctly guessed that they didn't exactly follow through the original idea.
    
    You are now to create a function that returns a Josephus permutation, taking as parameters the initial array/list of items to be permuted as if they were in a circle and counted out every k places until none remained.
    
    Tips and notes: it helps to start counting from 1 up to n, instead of the usual range 0..n-1; k will always be >=1.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+Josephus.JosephusPermutation(new List<object> { "C", "o", "d", "e", "W", "a", "r", "s" }, 4);
+
+public class Josephus
+{
+    public static List<object> JosephusPermutation(List<object> items, int k)
+    {
+
+        var resultArr = new List<object>();
+        var isFirst = true;
+        var index = 0;
+
+
+        
+
+        while (items.Count > 0)
+        {
+            index = checkIndex(items, k, index, isFirst);
+            isFirst = false;
+            resultArr.Add(items[index]);
+            items.RemoveAt(index);
+            index--;
+
+        }
+
+        static int checkIndex(List<object> items, int k, int startIndex, bool isFirst)
+        {
+
+            if (isFirst)
+            {
+                startIndex += (k-1) ;
+                isFirst = false;
+
+            }
+            else
+            {
+                startIndex += k;
+            }
+
+            while (startIndex >= items.Count)
+            {
+                startIndex -= items.Count;
+            }
+            return startIndex;
+        }
+
+        resultArr.ForEach(a =>
+        {
+            Console.WriteLine(a);
+        });
+        return resultArr;
+    }
+}
+
